45c1cfc8d59b69a72fc3d88152986d211c1292fb
Aditiyo7 <aditiyo.nugroho7@gmail.com>
2024-05-04 10:29:58 +0700

    Homework Web Automation Cucumber

--- features/pageobjects/sauce-cart.page.js
+++ features/pageobjects/sauce-cart.page.js
@@ -1,13 +1,13 @@
 const { $, browser } = require('@wdio/globals')
 
 class CartPage {
-
-    get checkoutButton() {
-        return $('#checkout');
+    
+    get shoppingCartList() {
+        return $('//*[@data-test="item-quantity"]')
     }
 
     async validateOnPage() {
-        await expect(this.checkoutButton).toHaveText("Checkout")
+        await expect(this.shoppingCartList).toHaveText("1")
         await browser.pause(3000)
     }
 }
